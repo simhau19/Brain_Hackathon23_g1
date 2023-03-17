1a6364615cb37a7caf7cf5bf112fe3ffc1be2e8b
Simen Haug <simehaug@stud.ntnu.no>
2023-03-17 22:39:41 +0100

    https working

--- Python/http_test.py
+++ Python/http_test.py
@@ -1,8 +1,19 @@
 import requests
 
 url = 'https://g1.cioty.com/test1'
-myobj = {'somekey': 'somevalue'}
 
-x = requests.post(url, json = myobj)
+headers = {
+    "Content-Type":"application/x-www-form-urlencoded",
+    "Synx-Cat":"4"
+}
 
-print(x.text)
+
+data = {
+    'token': 'aToken_36d8715e3531fd8e8c01fcbfd26bf5af1908e14f15014d2d14817b568bc0bb0e',
+    'objectID': '1',
+}
+
+with requests.post(url, headers=headers, data=data, stream=True) as response:
+    for line in response.iter_lines():
+        if line:
+            print(line.decode('utf-8'))
